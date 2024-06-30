c0e0a72b36faf83f575280debe9746bb4c148e31
Sidhesh-nishad <155049903+Sidhesh-nishad@users.noreply.github.com>
2024-06-30 11:20:51 +0530

    push

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -85,7 +85,7 @@ const Footer = () => {
                         type="text"
                         className="px-4 py-2 md:py-3 mt-4 border text-xs rounded-l-lg border-secondary outline-none max-w-[18.81rem]"
                     />
-                    <button className="md:p-3 py-2 px-3 text-xs font-semibold leading-4 border rounded-r-lg text-white border-secondary bg-secondary tracking-[0.06px]">
+                    <button className="md:p-3 py-2 px-2 text-xs font-semibold leading-4 border rounded-r-lg text-white border-secondary bg-secondary tracking-[0.06px]">
                         Invite
                     </button>
                 </div>
